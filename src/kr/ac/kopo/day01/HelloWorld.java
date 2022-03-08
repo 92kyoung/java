2a2e8d5fa03c235d5386f2e58eee7d470c014c5a
yikyoung <dowei22@naver.com>
2022-03-08 12:05:31 +0900

    hi

---
+++ src/kr/ac/kopo/day01/HelloWorld.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.day01;
+
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("hello world");
+
+		
+		
+		
+	}
+
+	
+}
---
+++ src/kr/ac/kopo/day01/PrintMain.java
@@ -0,0 +1,19 @@
+package kr.ac.kopo.day01;
+public class PrintMain {
+	public static void main(String args[]) {
+		
+		/*
+		 출력메소드
+		 1. println : 개행문자 포함
+		 2. print: 개행문자 불포함 
+		 */
+		
+		System.out.println(10);
+		System.out.println('A');
+		System.out.println(true);
+		System.out.println("tk"+1+5);
+		
+		
+	}
+
+}
