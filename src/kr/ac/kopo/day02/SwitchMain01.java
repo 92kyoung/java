b182b6d6fbbc5e115df9f20854d3e333a46687ff
yikyoung <dowei22@naver.com>
2022-03-10 15:30:09 +0900

    0310

--- src/kr/ac/kopo/day02/SwitchMain01.java
+++ src/kr/ac/kopo/day02/SwitchMain01.java
@@ -6,9 +6,22 @@ public static void main(String[] args) {
 	
 	Scanner sc = new Scanner (System.in);
 	
-	System.out.println("1~3 사이의 정수 입력: ");
+	System.out.print("1~3 사이의 정수 입력: ");
 	int num = sc.nextInt();
 	
+	switch(num) {
+	case 1:
+		System.out.println("one");
+		break;
+	case 2:
+		System.out.println("two");
+		break;
+	case 3:
+		System.out.println("three");
+		break;
+	default : //else의 의미 
+		System.out.println("error");
+	}
 	
 	
 	
