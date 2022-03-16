5ffb8ba563c119cacf392c9943ddcb8f2f743277
yikyoung <dowei22@naver.com>
2022-03-16 16:01:39 +0900

    DAY06(0316)

--- src/kr/ac/kopo/day06/Gugudan.java
+++ src/kr/ac/kopo/day06/Gugudan.java
@@ -1,6 +1,16 @@
 package kr.ac.kopo.day06;
 
+import java.util.Scanner;
+
 public class Gugudan {
+	
+	int getDan(){ //키보드 입력받을거여서 매개변수 없음
+		Scanner sc = new Scanner(System.in);
+		int dan =sc.nextInt();
+		return dan;
+	}
+	
+	
 	void print(int dan) {
 		System.out.println("***"+dan+"단 ***");
 		for(int i=1; i<=9; i++) {
--- src/kr/ac/kopo/day06/MethodMain04.java
+++ src/kr/ac/kopo/day06/MethodMain04.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 public class MethodMain04 {
 	public static void main(String[] args) {
+		Gugudan gu = new Gugudan();
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("2-9단 사이의 단을 입력: ");
-		int dan=sc.nextInt();
+		int dan=gu.getDan();
 		
-		Gugudan gu = new Gugudan();
 		
 		gu.print(dan);
 		gu.print();
 		
+		
+		System.out.print("시작단을 입력하시오:");
+		int startDan= gu.getDan();
+		System.out.print("끝단을 입력하시오:");
+		int endDan= gu.getDan();
+		
+		System.out.println(startDan+":"+endDan);
 	}
 }
