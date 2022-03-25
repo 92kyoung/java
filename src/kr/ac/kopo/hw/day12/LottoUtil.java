a5f4fa9f863d8c2ebdec95a11ef11eac505cc10d
yikyoung <dowei22@naver.com>
2022-03-25 15:50:24 +0900

    0325

---
+++ src/kr/ac/kopo/hw/day12/LottoUtil.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.hw.day12;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class LottoUtil {
+	
+	public void start() {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("게임 횟수를 입력하세요");
+		int cnt = sc.nextInt();
+	        for(int i = 0; i < cnt; i++) {
+	          Arrays.toString(getNumbers());
+		     //System.out.println(gameArr);
+		          //Arrays.toString() //배열 안 내용 출력하기
+	        }
+
+	    }
+
+	    private int[] getNumbers() {
+		// 1 ~ 45 사이의 로또번호 6개 추출
+	    
+	    //1번방법
+	    	Math.random()*
+	    	
+	    	
+	    	
+	    	return 배열값
+	    }
+}
